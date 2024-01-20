82f20148954f0067014d82234d9f8d064238164b
Stanislaw Horna R9 WSL <stanislawhorna@outlook.com>
2024-01-20 22:20:10 +0100

    modified:   Hello.ps1

--- Hello.ps1
+++ Hello.ps1
@@ -1,4 +1,7 @@
-Write-Host "Hello!"
-Write-Host "It's $(Get-Date)"
+for ($i = 0; $i -lt 10; $i++) {
+    Write-Host "Hello!"
+    Write-Host "It's $($(Get-Date).ToString("yyyy-MM-dd HH:mm:ss"))"
+    Start-Sleep -Milliseconds 500
+}
 
 exit 12
